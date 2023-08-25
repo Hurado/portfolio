44b90517e5d0dbf9c02c89d71588decb24ab5841
Hurado <piaszczynskiarkadiusz@gmail.com>
2023-08-25 13:16:57 +0200

    Added proffile photo, description, changed layout for intro section

---
+++ src/components/skills/skills.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './skills.css'
+import UIDesign from '../../assets/ui-design.png'
+import WebDesign from '../../assets/website-design.png'
+import AppDesign from '../../assets/app-design.png'
+
+const Skills = () => {
+  return (
+    <section id = 'skills'>
+        About section
+    </section>
+  )
+}
+
+export default Skills
