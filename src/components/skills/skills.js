c115a038091cc2d600c0a32554c0ad77a520b1f7
Arkadiusz Piaszczynski <piaszczynskiarkadiusz@gmail.com>
2023-08-25 20:38:25 +0200

    Added skills section with layout and content

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -7,7 +7,32 @@ import AppDesign from '../../assets/app-design.png'
 const Skills = () => {
   return (
     <section id = 'skills'>
-        About section
+        <span className="skillTitle">What I do</span>
+        <span className="skillDesc">I am ambitious and passionate web designer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, Javascript, React.
+I like learning new things, which is currently NextJs, databases and Java </span>
+<div className="skillBars">
+  <div className="skillBar">
+    <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
+    <div className="skillBarText">
+      <h2>UI/UX Design</h2>
+      <p>This is a demo text, you can write your own content here.</p>
+    </div>
+  </div>
+  <div className="skillBar">
+    <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
+    <div className="skillBarText">
+      <h2>Web Design</h2>
+      <p>This is a demo text, you can write your own content here.</p>
+    </div>
+  </div>
+  <div className="skillBar">
+    <img src={AppDesign} alt="AppDesign" className="skillBarImg" />
+    <div className="skillBarText">
+      <h2>App Design</h2>
+      <p>This is a demo text, you can write your own content here.</p>
+    </div>
+  </div>
+</div>
     </section>
   )
 }
