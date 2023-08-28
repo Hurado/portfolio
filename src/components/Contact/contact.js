f60d809b332e63f95c2a0f20b5c42cac14eca039
Arkadiusz Piaszczynski <piaszczynskiarkadiusz@gmail.com>
2023-08-28 16:50:41 +0200

    Added EmailJS service. First MVP ends.

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import SF from '../../assets/studio-figura.png'
 import IBPM from '../../assets/IBPM.png'
@@ -6,10 +6,23 @@ import OSKAR from '../../assets/OSKAR.png'
 import LaboPrint from '../../assets/LaboPrint.png'
 import LinkedinIcon from '../../assets/linkedin.png'
 import GithubIcon from '../../assets/github.png'
-
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+          .then((result) => {
+              console.log(result.text);
+              e.target.reset();
+              alert('Email sent!')
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
   return (
     <section id="contactPage">
         <div id="clients">
@@ -27,9 +40,9 @@ const Contact = () => {
         <div id="contact">
             <h1 className="contactPageTitle">Contact me</h1>
             <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
-            <form action="" className="contactForm">
-                <input type="text" className="name" placeholder='Your Name' />
-                <input type="email" className="email" placeholder='Your Email' />
+            <form action="" className="contactForm" ref={form} onSubmit={sendEmail}>
+                <input type="text" className="name" placeholder='Your Name' name='your_name' />
+                <input type="email" className="email" placeholder='Your Email' name='your_email' />
                 <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
                 <button type='submit' value= 'Send' className="submitBtn">Submit</button>
                 <div className="links">
