68a20598b6e08006d27c41e466b2776b937dc8e8
Arkadiusz Piaszczynski <piaszczynskiarkadiusz@gmail.com>
2023-09-04 18:26:52 +0200

    Changed experience section with fixed layout

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -3,26 +3,33 @@ import { VerticalTimeline,VerticalTimelineElement } from 'react-vertical-timelin
 import 'react-vertical-timeline-component/style.min.css';
 import {styles} from './styles';
 import { experiences } from "../../constants/json.js";
-
+import './experience.css'
 
 
 const ExperienceCard = ({experience}) => (
 
   <VerticalTimelineElement
     contentStyle = {{ background:'#1d1836', color: '#fff'}}
-    contentArrowStyle={{borderRight: '7px solid #232631'}} date={experience.date} iconStyle={{background:experience.iconBg}} icon={
-      <div className='flex justify-center items-center w-full h-full'>
-        <img src={experience.icon}  alt={experience.company_name} className="w-[60%] h-[60] object-contain" />
+    contentArrowStyle={{
+      borderRight: '7px solid #232631'
+    }} 
+    date={experience.date} iconStyle={{
+      background:experience.iconBg
+    }} icon={
+      <div 
+      className='experience-container'
+      >
+        <img src={experience.icon}  alt={experience.company_name} className="image-exp" />
       </div>
     }
     >
       <div>
-        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
-        <p className='text-secondary text-[16px] font-semibold' style={{margin:0}}>{experience.company_name}</p>
+        <h3 className='exp-title'>{experience.title}</h3>
+        <p className='paragraph_title' style={{margin:0}}>{experience.company_name}</p>
       </div>
 
-      <ul className='mt-5 list-disc ml-5 space-y-2'>{experience.points.map((point,index) => (
-        <li key={`experience-point-${index}`} className="text-white-100 text-[14px] pl-1 tracking-wider">
+      <ul className='exp-points'>{experience.points.map((point,index) => (
+        <li key={`experience-point-${index}`} className="point-element">
           {point}
         </li>
       ))}
@@ -35,7 +42,7 @@ const Experience = () => {
     <>
       <p className={styles.sectionSubText}>What i have done so far?</p>
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
-      <div className='mt-20 flex flex-col'>
+      <div className='section-wrapper'>
         <VerticalTimeline>
           {experiences.map((experience, index) => (
             <ExperienceCard key={index} experience= {experience}/>
